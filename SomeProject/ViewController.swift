672b57bf944f45fec9c1e0bc864e9c06db7a8aec
George Navarro <george@navarro.cc>
2018-01-02 21:57:21 -0800

    Initial Commit

---
+++ SomeProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SomeProject
+//
+//  Created by George Navarro on 1/2/18.
+//  Copyright © 2018 Seventh Bit Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
